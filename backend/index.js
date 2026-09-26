33cdeccde1d927b938cc4a18e1f54db82054ebee
Raghav Goyal <raghavgoyal1949@gmail.com>
2026-09-26 11:07:24 +0530

    MongoDB Connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+async function connectDB(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/git-learn");
+    console.log("Mongo DB conncted");
+}
+
+connectDB();
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
-
-
-
 
 
 
