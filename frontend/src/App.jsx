d63fad94fe27359cda3fa76ce2bc2ed1a86f007c
Raghav Goyal <raghavgoyal1949@gmail.com>
2026-09-26 11:47:57 +0530

    Basic frontend write

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task,setTask] = useState("");
+  const [todoList,setTodoList] = useState([]);
+  
+
+  function fun(event){
+    setTask(event.target.value);
+  }
+
+  function addTask(){
+    setTodoList([...todoList,{task : task,id : todoList.length+1}]);
+    setTask("");
+  }
+
 
   return (
     <>
+      <h1>Todo List</h1>
+    
+      <input type="text" placeholder="Add a task" value={task} onChange={fun}></input>&nbsp;&nbsp;
+      <button onClick={addTask}>Add Task</button>
+      <hr></hr>
+
+      <ul>
+        {todoList.map((item)=>(
+          <>
+          <li><b>{item.task}</b> <button>Delete</button></li>
+    
+          </>
+        ))}
+      </ul>
       
-          <h1>Get started</h1>
-          
     </>
   )
 }
